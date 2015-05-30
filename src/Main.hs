f4fd942b4ce5ce4f91b58510c0f76fc0814c3b78
Callum Rogers <callumr@palantir.com>
2015-05-30 10:15:20 -0700

    Flesh out reactive-banana stuff

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,34 @@
 module Main where
 
+import Control.Monad (replicateM_, forever)
+import Reactive.Banana
+import Reactive.Banana.Frameworks
 import System.Console.Terminal.Size (size, Window(..))
+import System.IO (BufferMode(..), hSetEcho, hSetBuffering, stdin)
 
 main :: IO ()
 main = do
-  (Just (Window h w)) <- size
-  putStrLn $ show h
-  putStrLn $ show w
+  turnOffInputBuffering
+  (addKeyEvent, fireKey) <- newAddHandler
+  network <- compile $ makeNetworkDescription addKeyEvent
+  actuate network
+  forever (getChar >>= fireKey)
+
+makeNetworkDescription :: Frameworks t => AddHandler Char -> Moment t ()
+makeNetworkDescription addKeyEvent = do
+  keyEvents <- fromAddHandler addKeyEvent
+  numberOfKeyPresses <- changes $ eventCounter keyEvents
+  reactimate' $ fmap (putStrLn . show) <$> numberOfKeyPresses
+
+eventCounter :: Event t a -> Behavior t Int
+eventCounter = accumB 0 . fmap (const (+1))
+
+turnOffInputBuffering :: IO ()
+turnOffInputBuffering = do
+  hSetEcho stdin False
+  hSetBuffering stdin NoBuffering
+
+scroll :: IO ()
+scroll = do
+  (Just (Window h _)) <- size
+  replicateM_ h $ putStrLn ""
