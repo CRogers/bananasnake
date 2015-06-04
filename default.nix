a071b9493a1a85863f5ecabb7440470be3655c9b
Callum Rogers <callumr@palantir.com>
2015-06-04 14:37:15 +0100

    Add random package

--- default.nix
+++ default.nix
@@ -1,10 +1,12 @@
-{ mkDerivation, base, reactive-banana, stdenv, terminal-size }:
+{ mkDerivation, base, random, reactive-banana, stdenv
+, terminal-size
+}:
 mkDerivation {
   pname = "bananasnake";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  buildDepends = [ base reactive-banana terminal-size ];
+  buildDepends = [ base random reactive-banana terminal-size ];
   license = stdenv.lib.licenses.mit;
 }
